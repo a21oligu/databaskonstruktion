36ecb5553ea98cb1d6ed2834d766df5f4f885eb0
Oliver Gustafsson <oliver.gustafsson17@gmail.com>
2022-09-19 15:11:35 +0200

    trigger now using views for cap check

--- sql/a21oligu.sql
+++ sql/a21oligu.sql
@@ -4,6 +4,11 @@ create database a21oligu;
 use a21oligu;
 
 /*CREATE USER "admin"@"%" IDENTIFIED BY "password";*/
+CREATE USER IF NOT EXISTS "tomtefar"@"%" IDENTIFIED BY "tomten";
+CREATE USER IF NOT EXISTS "admin"@"%" IDENTIFIED BY "admin";
+
+# Ge användaren tomtefar alla privilegier
+GRANT ALL PRIVILEGES ON a21oligu.* TO "tomtefar"@"%";
 
 /* 
 Tomtens renar identifieras av deras rennummer (11 siffrors löpnummer) eller deras namn som i
@@ -133,32 +138,67 @@ CREATE TABLE RenPris (
     FOREIGN KEY (nr, klan, underart) REFERENCES Ren(nr, klan, underart)
 )ENGINE=INNODB;
 
-/* PROCEDURES */
+/* VIEWS */
 
+# Hämtar antal renar i ett givet spann
+CREATE VIEW AntalRenarISpann AS 
+SELECT COUNT(*) AS antal, Ren.spann
+FROM Ren
+GROUP BY Ren.spann;
+
+# Hämtar andel av kapacitet som är upptagen i ett spann
+CREATE VIEW AndelAvKapacitetISpann AS 
+SELECT Spann.namn as spann, AntalRenarISpann.antal / Spann.kapacitet as andel
+FROM AntalRenarISpann, Spann
+WHERE AntalRenarISpann.spann = Spann.namn;
+
+/* PROCEDURES */
+DELIMITER &&
+
+# Räkna ut antal renar i ett spann
+CREATE PROCEDURE SpannAntal(spann VARCHAR(32), OUT res INT)
+BEGIN
+	DECLARE antal INT;
+    
+	SELECT COUNT(*)
+    INTO antal
+    FROM Ren
+    WHERE Ren.spann = spann;
+    
+    SET res = antal;
+END &&
+
+# Räkne ut andel av kapacitet som är upptagen i ett spann
+CREATE PROCEDURE SpannAndel(spann VARCHAR(32), OUT andel REAL)
+BEGIN
+	DECLARE antal INT;
+    DECLARE maxCap INT; # Max kapacitet
+    
+    CALL SpannAntal(spann, antal); # Sätter antal med 
+    
+    SELECT antal / Spann.kapacitet
+    INTO andel
+    FROM Spann
+    WHERE spann = Spann.namn;
+    
+END &&
 
 /* RULE CHECKING TRIGGERS */
 
-DELIMITER &&
+
 /* Kollar om ett spanns kapacitet är uppnåd innan en insert */
-CREATE TRIGGER SpannKapacitetCheck 
-BEFORE INSERT 
-ON Ren FOR EACH ROW 
+CREATE TRIGGER SpannKapacitetCheck BEFORE INSERT ON Ren 
+FOR EACH ROW 
 BEGIN
-	DECLARE rowCount smallint;
-    DECLARE maxCapacity smallint;
+	DECLARE percentage REAL;
     
-    SELECT COUNT(*)
-    INTO rowCount
-    FROM Ren
-    WHERE Ren.spann = NEW.spann;
+    SELECT andel 
+    INTO percentage
+    FROM AndelAvKapacitetISpann
+    WHERE AndelAvKapacitetISpann.spann = NEW.spann;
     
-    SELECT kapacitet
-    INTO maxCapacity
-    FROM Spann
-    WHERE Spann.namn = NEW.spann;
-    
-	IF rowCount >= maxCapacity THEN
-		SET @message = concat(NEW.spann, " has reached its max capacity of ", maxCapacity);
+	IF percentage >= 1 THEN
+		SET @message = concat(NEW.spann, " has reached its maximum capacity");
 		SIGNAL SQLSTATE "45000" set message_text = @message;
     END IF;
 END &&
@@ -171,18 +211,20 @@ INSERT INTO Tillverkare VALUES (0, "Scan");
 INSERT INTO Stank VALUES (5, "Skit");
 
 INSERT INTO Spann VALUES ("spann1", 3);
+INSERT INTO Spann VALUES ("spann2", 1);
 
 INSERT INTO Ren(nr, klan, underart, stank, spann, lön, typ) values (0, "klan1", "buskensis", 5, "spann1", 2000, "tjänste");
 
 /* INSERT INTO Ren values (0, "klan1", "fails", "skit", "spann1"); /* SHOULD FAIL */
-INSERT INTO Mat VALUES ("Falukorv", 0, 200);
+INSERT INTO Mat VALUES ("Falukorv", 0, 3);
 INSERT INTO Mat VALUES ("Pölse", 0, 1337);
 INSERT INTO RenÄterMat VALUES ("Falukorv", 0, "klan1", "buskensis");
 /* INSERT INTO RenÄterMat VALUES ("Pölse", 0, "buskensis", "kung"); /* SHOULD FAIL */
 
 INSERT INTO Ren(nr, klan, underart, stank, spann, lön, typ) values (1, "klan1", "buskensis", 5, "spann1", 2000, "tjänste");
-INSERT INTO Ren(nr, klan, underart, stank, spann, lön, typ) values (2, "klan1", "buskensis", 5, "spann1", 2000, "tjänste");
+INSERT INTO Ren(nr, klan, underart, stank, spann, lön, typ) values (2, "klan1", "buskensis", 5, "spann2", 2000, "tjänste");
+INSERT INTO Ren(nr, klan, underart, stank, spann, lön, typ) values (3, "klan1", "buskensis", 5, "spann1", 2000, "tjänste");
 
-SELECT *
-FROM Ren
-WHERE Ren.spann = "spann1";
+SELECT * FROM AntalRenarISpann;
+
+SELECT * FROM AndelAvKapacitetISpann WHERE spann = "spann2";
